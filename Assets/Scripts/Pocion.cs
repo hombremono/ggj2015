a0a41d452046645ac1e798478b3002a257647518
hombremono <nicom360@gmail.com>
2015-01-24 04:21:41 -0300

    D

---
+++ Assets/Scripts/Pocion.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pocion : MonoBehaviour {
+
+	public Stats stat;
+	public string NOMBRE_AGREGAR_POCION;
+	public string NOMBRE_PROBETA;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnLeftClick(){
+		Debug.Log("Pressed left click on pota.");
+		var obj1 = GameObject.Find (NOMBRE_PROBETA);
+		obj1.SendMessage (NOMBRE_AGREGAR_POCION, stat);
+	}
+
+	void OnRightClick(){
+		Debug.Log("Pressed right click on pota.");
+	}
+}
+
+public enum Stats {
+	Fuerza,
+	Destreza,
+	Inteligencia,
+	Carisma,
+	Suerte
+}
---
+++ Assets/Scripts/Probeta.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Probeta : MonoBehaviour {
+
+
+	private int EstadoActual;
+	private int fuerza;   
+	private int destreza;
+	private int inteligencia;
+	private int suerte;
+	private int carisma;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		EstadoActual = 0;
+		//--------
+
+		fuerza = 0;
+		destreza = 0;
+		inteligencia = 0;
+		suerte = 0;
+		carisma = 0;
+
+		//----------
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+
+	public void AgregarPota (Stats stat)
+	{
+		if (EstadoActual < 5) 
+		{
+			Debug.Log ("pota agregada");
+			if (stat == Stats.Fuerza)
+			{
+				fuerza++;
+				EstadoActual++;
+				transform.Find ("relleno" + EstadoActual).GetComponent<SpriteRenderer> ().color = new Color (0.84f,0.17f,0.3f,1f);
+			}
+			
+			if (stat == Stats.Destreza) 
+			{
+				destreza++;
+				EstadoActual++;
+				transform.Find ("relleno" + EstadoActual).GetComponent<SpriteRenderer> ().color = new Color (0.74f,0.36f,0.82f,1f);
+				
+			}
+			if (stat == Stats.Inteligencia) 
+			{
+				inteligencia++;
+				EstadoActual++;
+				transform.Find ("relleno" + EstadoActual).GetComponent<SpriteRenderer> ().color = new Color (0.46f,0.62f,0.84f,1f);
+			}
+			if (stat == Stats.Suerte) 
+			{
+				suerte++;
+				EstadoActual++;
+				transform.Find ("relleno" + EstadoActual).GetComponent<SpriteRenderer> ().color = new Color (0.74f,0.93f,0.14f,1f);
+			}
+			if (stat == Stats.Carisma) 
+			{
+				carisma++;
+				EstadoActual++;
+				transform.Find ("relleno" + EstadoActual).GetComponent<SpriteRenderer> ().color = new Color (1f,0.7f,0.2f,1f);
+			}
+			
+		}
+
+	}
+}
