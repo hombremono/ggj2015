a0ffb282234c7de021a70f9a9b93085bec4879ca
hombremono <nicom360@gmail.com>
2015-01-24 05:26:35 -0300

    no message

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+	public float roundTime;
+
+	// Use this for initialization
+	void Start () {
+		roundTime = 10;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (roundTime > 0) 
+		{
+			roundTime -= Time.deltaTime;
+			GetComponent<Text> ().text = roundTime.ToString("00.00")+"'";
+		}
+		else 
+		{
+			GetComponent<Text> ().text = "muerto";
+		}
+
+	
+	}
+}
