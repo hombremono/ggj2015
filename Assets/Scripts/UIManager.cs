43bde3dd48ab1718a61e749a1cedcfff4cd75b55
Marcelo Obachi <marcelo.obachi@gmail.com>
2015-01-24 09:52:38 -0300

    menu menu =)

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+	public void StartGame()
+	{
+		//Application.LoadLevel("RocketMouse");
+		Debug.Log ("pimba pimba!");
+	}	
+}
