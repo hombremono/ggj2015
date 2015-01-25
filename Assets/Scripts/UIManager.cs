ac066282006c372fc1c59efa31cc139872aea372
Marcelo Obachi <marcelo.obachi@gmail.com>
2015-01-25 09:09:54 -0300

    no message

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,8 +2,38 @@
 using System.Collections;
 
 public class UIManager : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.loadedLevelName == "tuto" && FindObjectsOfType<Pocion>().Length == 0)
+            Application.LoadLevel("nivel1");
+    }
+
+
 	public void StartGame()
 	{
 		Application.LoadLevel("tuto");
-	}	
+	}
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Nivel1()
+    {
+        Application.LoadLevel("nivel1");
+    }
+
+    public void Menu()
+    {
+        Application.LoadLevel("menu");
+    }
 }
